fe1ac9f3f47012a954c06627d97427ac33ab9b98
OUSMANE MAHAMAT SAMIT <2010215504@ogrenci.karabuk.edu.tr>
2023-04-20 19:19:47 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Samit_For_Entertainment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
